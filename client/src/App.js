3d9f8dabda024aeb140e1e60b9183580c675aa60
Harald Kjørholt <haraldkjorholt@gmail.com>
2022-01-22 00:30:19 +0100

    add images

--- client/src/App.js
+++ client/src/App.js
@@ -3,22 +3,32 @@ import logo from "./lawyerhat.svg";
 import "./App.css";
 
 function App() {
-  const [data, setData] = React.useState(null);
+    const [data, setData] = React.useState(null);
 
-  React.useEffect(() => {
-    fetch("/api")
-        .then((res) => res.json())
-        .then((data) => setData(data.message));
-  }, []);
+    React.useEffect(() => {
+        fetch("/api")
+            .then((res) => res.json())
+            .then((data) => setData(data.message));
+    }, []);
 
-  return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{!data ? "Er Marit advokat?" : data}</p>
-        </header>
-      </div>
-  );
+    return (
+        <div className="App">
+            <div className="header">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1>{!data ? "Er Marit advokat?" : data}</h1>
+                    <h6>Niks</h6>
+                </header>
+            </div>
+            <div class="body">
+                <body>
+                <a href="https://github.com/Rallstad/ermaritadvokat">Sjekk ut Haralds Github for massevis av uferdig ræl
+                    <img src="github.png" className="Link-image"></img>
+                </a>
+                </body>
+            </div>
+        </div>
+    );
 }
 
 export default App;
